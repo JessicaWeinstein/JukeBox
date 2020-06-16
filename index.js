602023ad5c8652290cd3beab968be1ddc60a309d
Jessica Weinstein <jessicaweinstein@Jessicas-MacBook-Pro.local>
2020-06-16 17:26:19 -0400

    added notes and color fill image when button is clicked

--- index.js
+++ index.js
@@ -26,35 +26,63 @@ nextButton.addEventListener("click", nextSong);
 
 function playSong(){
 	musicPlayer.play();	
+	playButton.src = "icons/play_White_Fill.png";
+	pauseButton.src = "icons/pause_White.png";
+	stopButton.src = "icons/stop_White.png";
+	previousButton.src = "icons/rewind_White.png";
+	nextButton.src = "icons/FF_White.png";
 }
 
 function pauseSong() {
 	musicPlayer.pause();
+	pauseButton.src = "icons/pause_White_Fill.png";
+	playButton.src = "icons/play_White.png";
+	stopButton.src = "icons/stop_White.png";
+	previousButton.src = "icons/rewind_White.png";
+	nextButton.src = "icons/FF_White.png";
 }
 
 function stopSong() {
 	musicPlayer.pause();
 	musicPlayer.currentTime = 0
+	stopButton.src = "icons/stop_White_Fill.png";
+	pauseButton.src = "icons/pause_White.png";
+	playButton.src = "icons/play_White.png";
+	previousButton.src = "icons/rewind_White.png";
+	nextButton.src = "icons/FF_White.png";
 }
 
 function previousSong(){
 	if (i === 0){
-		i = songs.length -1
+		i = songs.length -1 //last song in array
 	}
 	else{
-		i--;
+		i--; // if not first song it goes to previous index in array
 	}
-	musicPlayer.src = songs[i];
+	musicPlayer.src = songs[i]; 
 	musicPlayer.play();
+
+	previousButton.src = "icons/rewind_White_Fill.png";
+	stopButton.src = "icons/stop_White.png";
+	pauseButton.src = "icons/pause_White.png";
+	playButton.src = "icons/play_White.png";
+	nextButton.src = "icons/FF_White.png";
 }
 
 function nextSong(){
-	if (i === songs.length -1){
+	if (i === songs.length -1){ // sets i to first song if you're on the last song
 		i = 0
 	}
 	else{
-		i++;
+		i++; // adds 1 to the inidex in array
 	}
 	musicPlayer.src = songs[i];
 	musicPlayer.play();
+
+	nextButton.src = "icons/FF_White_Fill.png";
+	stopButton.src = "icons/stop_White.png";
+	pauseButton.src = "icons/pause_White.png";
+	playButton.src = "icons/play_White.png";
+	previousButton.src = "icons/rewind_White.png";
+	
 }
